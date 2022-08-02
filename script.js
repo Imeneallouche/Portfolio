22970ce13d4d9ae690d2a8edb54619f2b92b04cf
Imène ALLOUCJE <https://github.com/Imeneallouche>
2022-08-02 17:54:04 +0100

    responsive Home => nav and header (nav not completed yet) version 1.1

--- script.js
+++ script.js
@@ -1,4 +1,53 @@
-/*all links that got href in the nav bar( 5 links)*/
+/*Responsivity of the Nav Bar*/
+const navToggler = document.querySelector(".nav-toggler");
+const navMenu = document.querySelector("nav ul");
+const navLinks = navMenu.querySelectorAll("a");
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener("click", togglerClick);
+  // nav links click event
+  navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle("toggler-open");
+  navMenu.classList.toggle("open");
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if (navMenu.classList.contains("open")) {
+    navToggler.click();
+  }
+}
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+all links that got href in the nav bar( 5 links)*/
 let NavLinks = Array.from(document.querySelectorAll("a"));
 NavLinks = NavLinks.filter((link) => link.href.includes("#"));
 NavLinks.forEach((link) => link.addEventListener("click", ScrollWithHref));
