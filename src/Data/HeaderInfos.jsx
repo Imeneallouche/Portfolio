cb1633d0fb859361fcd4b5fba6a4c17ce5a27e59
Imeneallouche <li_allouche@esi.dz>
2023-09-29 08:14:21 -0500

    CV Link has been adjusted

--- src/Data/HeaderInfos.jsx
+++ src/Data/HeaderInfos.jsx
@@ -5,5 +5,5 @@ export const HeaderInfos = {
   description:
     "a dedicated MERN stack web developer and freelancer currently in my third year at the prestigious Higher National School of Computer Science (ESI Algiers). In addition to my academic pursuits, I proudly serve as the Vice President of the Shellmates club. My passion lies in the intricate world of cybersecurity, where I actively engage as a CTF player at ShellSec team and cybersecurity enthusiast.",
   CVlink:
-    "https://drive.google.com/file/d/1ZTls9-GPu_LjfnL1RQ9U4OZns2q1RDQ0/view?usp=sharing",
+    "https://drive.google.com/file/d/1YVmI_dACePLam1fvkdMtqed-yebM480D/view?usp=sharing",
 };
